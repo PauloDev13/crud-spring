2749f82433c97b3a84b9e9384228490bcbb35da7
PgmAdmin <prmorais1302@gmail.com>
2023-08-20 18:05:02 -0300

    16: H2 Database and Connecting Angular in Spring API

--- src/main/java/com/prmorais/crudspring/model/Course.java
+++ src/main/java/com/prmorais/crudspring/model/Course.java
@@ -1,5 +1,6 @@
 package com.prmorais.crudspring.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -8,6 +9,7 @@ import lombok.Data;
 public class Course {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @JsonProperty("_id")
   private Long id;
 
   @Column(nullable = false, length = 150)
