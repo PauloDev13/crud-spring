c703a25cd4e32e73b489b7af4955c63e4cea7e47
Paulo Roberto De Morais <prmorais1302@gmail.com>
2023-08-22 13:06:20 -0300

    31 - Remove Course: API Spring HTTP DELETE

--- src/main/java/com/prmorais/crudspring/controller/CourseController.java
+++ src/main/java/com/prmorais/crudspring/controller/CourseController.java
@@ -44,4 +44,14 @@ public class CourseController {
             })
             .orElse(ResponseEntity.notFound().build());
   }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+    return courseRepository.findById(id)
+            .map(data -> {
+              courseRepository.deleteById(id);
+              return ResponseEntity.noContent().<Void>build();
+            })
+            .orElse(ResponseEntity.notFound().build());
+  }
 }
