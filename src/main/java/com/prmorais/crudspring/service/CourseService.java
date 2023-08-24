f18966bc6674dfe4d29d0203d9dd5d5adbaa2571
Paulo Roberto De Morais <prmorais1302@gmail.com>
2023-08-24 11:36:48 -0300

    40 - Handling Exceptions with Controller Advice

--- src/main/java/com/prmorais/crudspring/service/CourseService.java
+++ src/main/java/com/prmorais/crudspring/service/CourseService.java
@@ -1,5 +1,6 @@
 package com.prmorais.crudspring.service;
 
+import com.prmorais.crudspring.exception.RecordNotFoundException;
 import com.prmorais.crudspring.model.Course;
 import com.prmorais.crudspring.repository.CourseRepository;
 import jakarta.validation.Valid;
@@ -10,7 +11,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Validated
@@ -25,29 +25,29 @@ public class CourseService {
     return courseRepository.findAll();
   }
 
-  public Optional<Course> findById(@PathVariable @NotNull @Positive Long id) {
-    return courseRepository.findById(id);
+  public Course findById(@PathVariable @NotNull @Positive Long id) {
+    return courseRepository.findById(id)
+        .orElseThrow(() -> new RecordNotFoundException(id));
   }
 
   public Course create(@Valid Course course) {
     return courseRepository.save(course);
   }
 
-  public Optional<Course> update(@NotNull @Positive Long id, @Valid Course course) {
+  public Course update(@NotNull @Positive Long id, @Valid Course course) {
     return courseRepository.findById(id)
         .map(data -> {
           data.setName(course.getName());
           data.setCategory(course.getCategory());
           return courseRepository.save(data);
-        });
+        })
+        .orElseThrow(() -> new RecordNotFoundException(id));
   }
 
-  public boolean delete(@NotNull @Positive Long id) {
-    return courseRepository.findById(id)
-        .map(data -> {
-          courseRepository.deleteById(id);
-          return true;
-        })
-        .orElse(false);
+  public void delete(@NotNull @Positive Long id) {
+    courseRepository.delete(
+        courseRepository.findById(id)
+            .orElseThrow(() -> new RecordNotFoundException(id))
+    );
   }
 }
